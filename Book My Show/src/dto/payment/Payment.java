a33af517996ce9fdef811672d58c7991e3fd64a7
DIWAKAR_MISHRA_YODLEE_PC <diwakarmishra1990@gmail.com>
2024-12-23 10:31:54 +0530

    Book My Show

---
+++ Book My Show/src/dto/payment/Payment.java
@@ -0,0 +1,15 @@
+package dto.payment;
+
+public class Payment {
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    int paymentId;
+    //Other payment details
+}
